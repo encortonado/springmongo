82f6321c3107906c9807c3ed285d7d7d522eb121
Lucas Bortolo <lucasbortolo3@gmail.com>
2020-12-03 22:56:34 -0300

    Projeto Criado

---
+++ src/test/java/com/bortolo/springmongo/SpringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bortolo.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
