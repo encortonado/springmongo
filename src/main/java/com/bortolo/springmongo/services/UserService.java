81ce040bab5abd72553a7698d8a5bf561ed27471
Lucas Bortolo <lucasbortolo3@gmail.com>
2020-12-04 18:03:49 -0300

    Inserir User com POST

--- src/main/java/com/bortolo/springmongo/services/UserService.java
+++ src/main/java/com/bortolo/springmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bortolo.springmongo.domain.User;
+import com.bortolo.springmongo.dto.UserDTO;
 import com.bortolo.springmongo.repository.UserRepository;
 import com.bortolo.springmongo.services.exception.ObjectNotFoundException;
 
@@ -15,18 +16,25 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
-		
+
 		return repository.findAll();
-		
+
 	}
- 	
-	
+
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
-	
+
+	public User insert(User user) {
+
+		return repository.insert(user);
+
+	}
+
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
 }
