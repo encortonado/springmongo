c0a3c308c8a5a643e9ae5d2a49b34a9452230735
Lucas Bortolo <lucasbortolo3@gmail.com>
2020-12-05 10:51:54 -0300

    Consulta com @query

--- src/main/java/com/bortolo/springmongo/repository/PostRepository.java
+++ src/main/java/com/bortolo/springmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.bortolo.springmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.bortolo.springmongo.domain.Post;
 
@@ -10,6 +11,7 @@ public interface PostRepository extends MongoRepository<Post, String>{
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
-	
+	@Query("{'title' : { $regex ?0, $options : 'i'} }")
+	List<Post> findByTitle(String text); // faz a mesma coisa que o acima so que com @query
 	
 }
